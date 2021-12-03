594499151297ae7f970053c4300bd38ea3ead73d
KonorX <ruslan.bekchanov@gmail.com>
2021-12-04 01:19:07 +0300

    added Lab

---
+++ lab_first/lab_first.cpp
@@ -0,0 +1,327 @@
+// lab_first.cpp : Defines the entry point for the application.
+//
+
+#include "framework.h"
+#include "lab_first.h"
+#include <string>
+
+#define MAX_LOADSTRING 100
+
+// Global Variables:
+HINSTANCE hInst;                                // current instance
+WCHAR szTitle[MAX_LOADSTRING];                  // The title bar text
+WCHAR szWindowClass[MAX_LOADSTRING];            // the main window class name
+
+// Forward declarations of functions included in this code module:
+ATOM                MyRegisterClass(HINSTANCE hInstance);
+BOOL                InitInstance(HINSTANCE, int);
+LRESULT CALLBACK    WndProc(HWND, UINT, WPARAM, LPARAM);
+INT_PTR CALLBACK    About(HWND, UINT, WPARAM, LPARAM);
+
+int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
+                     _In_opt_ HINSTANCE hPrevInstance,
+                     _In_ LPWSTR    lpCmdLine,
+                     _In_ int       nCmdShow)
+{
+    UNREFERENCED_PARAMETER(hPrevInstance);
+    UNREFERENCED_PARAMETER(lpCmdLine);
+
+    // TODO: Place code here.
+
+    // Initialize global strings
+    LoadStringW(hInstance, IDS_APP_TITLE, szTitle, MAX_LOADSTRING);
+    LoadStringW(hInstance, IDC_LABFIRST, szWindowClass, MAX_LOADSTRING);
+    MyRegisterClass(hInstance);
+
+    // Perform application initialization:
+    if (!InitInstance (hInstance, nCmdShow))
+    {
+        return FALSE;
+    }
+
+    HACCEL hAccelTable = LoadAccelerators(hInstance, MAKEINTRESOURCE(IDC_LABFIRST));
+
+    MSG msg;
+
+    // Main message loop:
+    while (GetMessage(&msg, nullptr, 0, 0))
+    {
+        if (!TranslateAccelerator(msg.hwnd, hAccelTable, &msg))
+        {
+            TranslateMessage(&msg);
+            DispatchMessage(&msg);
+        }
+    }
+
+    return (int) msg.wParam;
+}
+
+
+
+//
+//  FUNCTION: MyRegisterClass()
+//
+//  PURPOSE: Registers the window class.
+//
+
+
+
+
+BYTE ANDmaskCursor[] =
+{
+    0xFF, 0xFC, 0x3F, 0xFF,   // line 1 
+    0xFF, 0xC0, 0x1F, 0xFF,   // line 2 
+    0xFF, 0x00, 0x3F, 0xFF,   // line 3 
+    0xFE, 0x00, 0xFF, 0xFF,   // line 4 
+
+    0xF7, 0x01, 0xFF, 0xFF,   // line 5 
+    0xF0, 0x03, 0xFF, 0xFF,   // line 6 
+    0xF0, 0x03, 0xFF, 0xFF,   // line 7 
+    0xE0, 0x07, 0xFF, 0xFF,   // line 8 
+
+    0xC0, 0x07, 0xFF, 0xFF,   // line 9 
+    0xC0, 0x0F, 0xFF, 0xFF,   // line 10 
+    0x80, 0x0F, 0xFF, 0xFF,   // line 11 
+    0x80, 0x0F, 0xFF, 0xFF,   // line 12 
+
+    0x80, 0x07, 0xFF, 0xFF,   // line 13 
+    0x00, 0x07, 0xFF, 0xFF,   // line 14 
+    0x00, 0x03, 0xFF, 0xFF,   // line 15 
+    0x00, 0x00, 0xFF, 0xFF,   // line 16 
+
+    0x00, 0x00, 0x7F, 0xFF,   // line 17 
+    0x00, 0x00, 0x1F, 0xFF,   // line 18 
+    0x00, 0x00, 0x0F, 0xFF,   // line 19 
+    0x80, 0x00, 0x0F, 0xFF,   // line 20 
+
+    0x80, 0x00, 0x07, 0xFF,   // line 21 
+    0x80, 0x00, 0x07, 0xFF,   // line 22 
+    0xC0, 0x00, 0x07, 0xFF,   // line 23 
+    0xC0, 0x00, 0x0F, 0xFF,   // line 24 
+
+    0xE0, 0x00, 0x0F, 0xFF,   // line 25 
+    0xF0, 0x00, 0x1F, 0xFF,   // line 26 
+    0xF0, 0x00, 0x1F, 0xFF,   // line 27 
+    0xF8, 0x00, 0x3F, 0xFF,   // line 28 
+
+    0xFE, 0x00, 0x7F, 0xFF,   // line 29 
+    0xFF, 0x00, 0xFF, 0xFF,   // line 30 
+    0xFF, 0xC3, 0xFF, 0xFF,   // line 31 
+    0xFF, 0xFF, 0xFF, 0xFF    // line 32 
+};
+
+BYTE XORmaskCursor[] =
+{
+    0x00, 0x00, 0x00, 0x00,   // line 1 
+    0x00, 0x03, 0xC0, 0x00,   // line 2 
+    0x00, 0x3F, 0x00, 0x00,   // line 3 
+    0x00, 0xFE, 0x00, 0x00,   // line 4 
+
+    0x0E, 0xFC, 0x00, 0x00,   // line 5 
+    0x07, 0xF8, 0x00, 0x00,   // line 6 
+    0x07, 0xF8, 0x00, 0x00,   // line 7 
+    0x0F, 0xF0, 0x00, 0x00,   // line 8 
+
+    0x1F, 0xF0, 0x00, 0x00,   // line 9 
+    0x1F, 0xE0, 0x00, 0x00,   // line 10 
+    0x3F, 0xE0, 0x00, 0x00,   // line 11 
+    0x3F, 0xE0, 0x00, 0x00,   // line 12 
+
+    0x3F, 0xF0, 0x00, 0x00,   // line 13 
+    0x7F, 0xF0, 0x00, 0x00,   // line 14 
+    0x7F, 0xF8, 0x00, 0x00,   // line 15 
+    0x7F, 0xFC, 0x00, 0x00,   // line 16 
+
+    0x7F, 0xFF, 0x00, 0x00,   // line 17 
+    0x7F, 0xFF, 0x80, 0x00,   // line 18 
+    0x7F, 0xFF, 0xE0, 0x00,   // line 19 
+    0x3F, 0xFF, 0xE0, 0x00,   // line 20 
+
+    0x3F, 0xC7, 0xF0, 0x00,   // line 21 
+    0x3F, 0x83, 0xF0, 0x00,   // line 22 
+    0x1F, 0x83, 0xF0, 0x00,   // line 23 
+    0x1F, 0x83, 0xE0, 0x00,   // line 24 
+
+    0x0F, 0xC7, 0xE0, 0x00,   // line 25 
+    0x07, 0xFF, 0xC0, 0x00,   // line 26 
+    0x07, 0xFF, 0xC0, 0x00,   // line 27 
+    0x01, 0xFF, 0x80, 0x00,   // line 28 
+
+    0x00, 0xFF, 0x00, 0x00,   // line 29 
+    0x00, 0x3C, 0x00, 0x00,   // line 30 
+    0x00, 0x00, 0x00, 0x00,   // line 31 
+    0x00, 0x00, 0x00, 0x00    // line 32 
+};
+
+HCURSOR newCursor = CreateCursor(
+    hInst,
+    19,
+    2,
+    32,
+    32,
+    ANDmaskCursor,
+    XORmaskCursor
+);
+
+ATOM MyRegisterClass(HINSTANCE hInstance)
+{
+    WNDCLASSEXW wcex;
+
+    wcex.cbSize = sizeof(WNDCLASSEX);
+
+    wcex.style          = CS_HREDRAW | CS_VREDRAW;
+    wcex.lpfnWndProc    = WndProc;
+    wcex.cbClsExtra     = 0;
+    wcex.cbWndExtra     = 0;
+    wcex.hInstance      = hInstance;
+    wcex.hIcon          = LoadIcon(hInstance, MAKEINTRESOURCE(IDI_ICON3));
+    //wcex.hIcon = LoadImage(NULL,"star.ico",IMAGE_ICON,0,0,LR_LO);
+    /*wcex.hCursor        = LoadCursor(nullptr, IDC_ARROW);*/
+    wcex.hCursor = newCursor;
+    wcex.hbrBackground  = (HBRUSH)(COLOR_WINDOW+1);
+    wcex.lpszMenuName   = MAKEINTRESOURCEW(IDC_LABFIRST);
+    wcex.lpszClassName  = szWindowClass;
+    wcex.hIconSm        = LoadIcon(wcex.hInstance, MAKEINTRESOURCE(IDI_ICON3));
+
+    return RegisterClassExW(&wcex);
+}
+
+//
+//   FUNCTION: InitInstance(HINSTANCE, int)
+//
+//   PURPOSE: Saves instance handle and creates main window
+//
+//   COMMENTS:
+//
+//        In this function, we save the instance handle in a global variable and
+//        create and display the main program window.
+//
+BOOL InitInstance(HINSTANCE hInstance, int nCmdShow)
+{
+   hInst = hInstance; // Store instance handle in our global variable
+
+   HWND hWnd = CreateWindowW(szWindowClass, szTitle, WS_OVERLAPPEDWINDOW,
+      CW_USEDEFAULT, 0, CW_USEDEFAULT, 0, nullptr, nullptr, hInstance, nullptr);
+
+   if (!hWnd)
+   {
+      return FALSE;
+   }
+
+   ShowWindow(hWnd, nCmdShow);
+   UpdateWindow(hWnd);
+
+   return TRUE;
+}
+
+int tries = 0;
+std::wstring s2ws(const std::string& s)
+{
+    int len;
+    int slength = (int)s.length() + 1;
+    len = MultiByteToWideChar(CP_ACP, 0, s.c_str(), slength, 0, 0);
+    wchar_t* buf = new wchar_t[len];
+    MultiByteToWideChar(CP_ACP, 0, s.c_str(), slength, buf, len);
+    std::wstring r(buf);
+    delete[] buf;
+    return r;
+}
+//
+//  FUNCTION: WndProc(HWND, UINT, WPARAM, LPARAM)
+//
+//  PURPOSE: Processes messages for the main window.
+//
+//  WM_COMMAND  - process the application menu
+//  WM_PAINT    - Paint the main window
+//  WM_DESTROY  - post a quit message and return
+//
+//
+LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
+{
+    
+    switch (message)
+    {
+    case WM_COMMAND:
+        {
+            int wmId = LOWORD(wParam);
+            // Parse the menu selections:
+            switch (wmId)
+            {
+            case IDM_ABOUT:
+                DialogBox(hInst, MAKEINTRESOURCE(IDD_ABOUTBOX), hWnd, About);
+                break;
+            case IDM_EXIT: {
+                tries++;
+                char buf[100];
+                std::string info = "Точно выходим?\n";
+
+                std::string str = std::to_string(tries);
+                std::string newStr = info + str;
+                std::wstring strtemp = s2ws(newStr);
+                LPCWSTR textstr = strtemp.c_str();
+                
+                int msgboxID = MessageBox(
+                    NULL,
+                    textstr,
+                    L"окошко",
+                    MB_ICONEXCLAMATION | MB_YESNO
+                );
+                
+                if (msgboxID == IDYES)
+                {
+                    DestroyWindow(hWnd);
+                    break;
+                }
+                
+                break;
+            }
+            default:
+                return DefWindowProc(hWnd, message, wParam, lParam);
+            }
+        }
+        break;
+    case WM_PAINT:
+        {
+            PAINTSTRUCT ps;
+            /*std::string str = std::to_string(tries);
+            std::wstring strtemp = s2ws(str);
+            LPCWSTR textstr = strtemp.c_str();*/
+            HDC hdc = BeginPaint(hWnd, &ps);
+            //TextOut(hdc, 10, 10, textstr, str.length());
+            // TODO: Add any drawing code that uses hdc here...
+            EndPaint(hWnd, &ps);
+        }
+        break;
+    case WM_DESTROY:
+        PostQuitMessage(0);
+        break;
+    default:
+        return DefWindowProc(hWnd, message, wParam, lParam);
+    }
+    return 0;
+}
+
+const char* addFoo(const char* strInput)
+{
+    return ("Точно выходим? \n" + std::string(strInput)).c_str();
+}
+
+// Message handler for about box.
+INT_PTR CALLBACK About(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam)
+{
+    UNREFERENCED_PARAMETER(lParam);
+    switch (message)
+    {
+    case WM_INITDIALOG:
+        return (INT_PTR)TRUE;
+
+    case WM_COMMAND:
+        if (LOWORD(wParam) == IDOK || LOWORD(wParam) == IDCANCEL)
+        {
+            EndDialog(hDlg, LOWORD(wParam));
+            return (INT_PTR)TRUE;
+        }
+        break;
+    }
+    return (INT_PTR)FALSE;
+}
---
+++ lab_first/resource.h
@@ -0,0 +1,37 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by lab_first.rc
+//
+#define IDC_MYICON                      2
+#define IDD_LABFIRST_DIALOG             102
+#define IDS_APP_TITLE                   103
+#define IDD_ABOUTBOX                    103
+#define IDM_ABOUT                       104
+#define IDM_EXIT                        105
+#define IDI_LABFIRST                    107
+#define IDI_SMALL                       108
+#define IDC_LABFIRST                    109
+#define IDR_MAINFRAME                   128
+#define IDI_ICON1                       131
+#define IDI_ICON2                       132
+#define IDI_ICON3                       133
+#define IDI_ICON4                       134
+#define IDI_ICON5                       136
+#define IDI_ICON6                       137
+#define IDR_NEWICON1                    138
+#define IDR_IDI_ICONTRUE1               139
+#define IDI_ICON7                       140
+#define IDI_ICON8                       141
+#define IDC_STATIC                      -1
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NO_MFC                     1
+#define _APS_NEXT_RESOURCE_VALUE        142
+#define _APS_NEXT_COMMAND_VALUE         32771
+#define _APS_NEXT_CONTROL_VALUE         1000
+#define _APS_NEXT_SYMED_VALUE           110
+#endif
+#endif
